f1f41191de0ea971d93b6f994f1d5517ee808b86
Robson Lopes Cavalcante <roblucci100@gmail.com>
2024-10-08 21:59:23 -0300

    refactor: conditionals and empty values

--- heroi-classificador/script.js
+++ heroi-classificador/script.js
@@ -1,32 +1,40 @@
 function classificarHeroi() {
   const nome = document.getElementById('nome').value;
   const xp = parseInt(document.getElementById('xp').value);
+
+  // Verifica se nome e xp são vazios, disparando um alerta
+  if(!nome || !xp) {
+    alert('Insira um herói antes de classificá-lo!');
+    return; // Faz com que a função encerre
+  }
+
   let nivel = '';
   let imagem = '';
 
   // Estrutura de decisão para determinar o nível do herói e a imagem correspondente
+  // Condições duplicadas removidas
   if (xp <= 1000) {
     nivel = 'Ferro';
     imagem = 'img/ferro.png';
-  } else if (xp >= 1001 && xp <= 2000) {
+  } else if (xp <= 2000) {
     nivel = 'Bronze';
     imagem = 'img/bronze.png';
-  } else if (xp >= 2001 && xp <= 5000) {
+  } else if (xp <= 5000) {
     nivel = 'Prata';
     imagem = 'img/prata.png';
-  } else if (xp >= 5001 && xp <= 7000) {
+  } else if (xp <= 7000) {
     nivel = 'Ouro';
     imagem = 'img/ouro.png';
-  } else if (xp >= 7001 && xp <= 8000) {
+  } else if (xp <= 8000) {
     nivel = 'Platina';
     imagem = 'img/platina.png';
-  } else if (xp >= 8001 && xp <= 9000) {
+  } else if (xp <= 9000) {
     nivel = 'Ascendente';
     imagem = 'img/ascendente.png';
-  } else if (xp >= 9001 && xp <= 10000) {
+  } else if (xp <= 10000) {
     nivel = 'Imortal';
     imagem = 'img/imortal.png';
-  } else if (xp >= 10001) {
+  } else {
     nivel = 'Radiante';
     imagem = 'img/radiante.png';
   }
